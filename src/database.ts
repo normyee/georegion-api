b9c65452995e48381a34e232ac3cc0adb2d39332
normyee <lunoqro@gmail.com>
2025-03-07 16:51:51 -0300

    feat(database): add connection check to ensure database is connected

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import { logger } from "./logger";
 
 const env = {
-  MONGO_URI: 'mongodb://root:example@127.0.0.1:27021/oz-tech-test?authSource=admin',
+  MONGO_URI:
+    "mongodb://root:example@127.0.0.1:27021/oz-tech-test?authSource=admin",
 };
 
-const init = async function() {
+const init = async function () {
   await mongoose.connect(env.MONGO_URI);
+
+  if (mongoose.connection.readyState === 1)
+    logger.info(`MongoDB connection is ready`);
 };
 
 export default init();
