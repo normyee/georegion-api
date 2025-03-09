d6110952bb74394545852b67c38d1d25dc2c763b
normyee <lunoqro@gmail.com>
2025-03-09 14:08:50 -0300

    feat: add update user repository

---
+++ src/database/repositories/users/update-user.repository.ts
@@ -0,0 +1,44 @@
+import { getModelForClass } from "@typegoose/typegoose";
+import { User } from "../../../models";
+import mongoose from "mongoose";
+import { User as UserEntity } from "../../../entity/user.model";
+
+export class UpdateUserRepositoryMongoAdapter {
+  private _user = getModelForClass(User);
+
+  public async execute(data: UserEntity): Promise<UserEntity> {
+    const session = await mongoose.startSession();
+    let updatedUser = null;
+
+    session.startTransaction();
+
+    try {
+      updatedUser = await this._user
+        .findOneAndUpdate({ _id: data.id }, data, {
+          new: true,
+          session,
+        })
+        .exec();
+
+      if (!updatedUser) {
+        throw new Error("User not found");
+      }
+
+      await session.commitTransaction();
+      session.endSession();
+
+      return new UserEntity(
+        updatedUser._id,
+        updatedUser.name,
+        updatedUser.email,
+        updatedUser.address,
+        updatedUser.coordinates
+      );
+    } catch (error) {
+      await session.abortTransaction();
+      session.endSession();
+
+      throw error;
+    }
+  }
+}
