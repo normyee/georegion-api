b31b77130db6ea52ba679c62739fde7efe6b6c2c
normyee <lunoqro@gmail.com>
2025-03-13 10:13:00 -0300

    feat: add index creation for geometry queries

--- src/geo-app/infra/database/orm/mongoose/models/region.model.ts
+++ src/geo-app/infra/database/orm/mongoose/models/region.model.ts
@@ -1,44 +1,32 @@
 import "reflect-metadata";
 import * as mongoose from "mongoose";
 import {
-  pre,
   Prop,
   Ref,
   modelOptions,
   getModelForClass,
 } from "@typegoose/typegoose";
-import ObjectId = mongoose.Types.ObjectId;
-import { User, UserModel } from "./user.model";
+import { User } from "./user.model";
 import { Base } from "./common/base.model";
 
-@pre<Region>("save", async function (next) {
-  const region = this as Omit<any, keyof Region> & Region;
-
-  if (!region._id) {
-    region._id = new ObjectId().toString();
-  }
-
-  if (region.isNew) {
-    const user = await UserModel.findOne({ _id: region.user });
-    user.regions.push(region._id);
-    await user.save({ session: region.$session() });
-  }
-
-  next(region.validateSync());
-})
 @modelOptions({ schemaOptions: { validateBeforeSave: false } })
 export class Region extends Base {
-  @Prop({ required: true, auto: true })
-  _id: string;
-
   @Prop({ required: true })
   name!: string;
 
   @Prop({ ref: "User", required: true, type: () => String })
   user: Ref<User>;
 
-  @Prop({ required: true, type: () => [[Number]] })
-  coordinates: [number, number][];
+  @Prop({
+    required: true,
+    type: mongoose.Schema.Types.Mixed,
+  })
+  geometry!: {
+    type: string;
+    coordinates: number[][][];
+  };
 }
 
 export const RegionModel = getModelForClass(Region);
+
+RegionModel.collection.createIndex({ geometry: "2dsphere" });
