ac5e8465d2bb8eea4f840f7d9935afcd84cd4f8e
normyee <lunoqro@gmail.com>
2025-03-14 10:41:16 -0300

    refactor(geo-app): create a geo-app-module for singleton handling

---
+++ src/geo-app/infra/geo-app.module.ts
@@ -0,0 +1,147 @@
+import { useContainer, useExpressServer } from "routing-controllers";
+import Container from "typedi";
+import { CreateUserUseCase } from "../application/use-case/user/create-user.use-case";
+import { CreateUserRepositoryMongoAdapter } from "./database/orm/mongoose/repositories/users/create-user.repository";
+import { GeoLibOpenStreetMapAdapter } from "./providers/geo/geo-lib.provider";
+import { NetworkAxiosAdapter } from "../../shared/network.provider";
+import { UserMapper } from "../application/common/mappers/user.mapper";
+import { AuthProvider } from "../../shared/auth.provider";
+import { DeleteUserUseCase } from "../application/use-case/user/delete-user-use-case";
+import { DeleteUserRepositoryMongoAdapter } from "./database/orm/mongoose/repositories/users/delete-user.repository";
+import { GetUserUseCase } from "../application/use-case/user/get-user.use-case";
+import { GetUserRepositoryMongoAdapter } from "./database/orm/mongoose/repositories/users/get-user.repository";
+import { GetAllUsersUseCase } from "../application/use-case/user/get-all-users.use-case";
+import { GetAllUsersRepositoryMongoAdapter } from "./database/orm/mongoose/repositories/users/get-all-users.repository";
+import { UpdateUserUseCase } from "../application/use-case/user/update-user.use-case";
+import { UpdateUserRepositoryMongoAdapter } from "./database/orm/mongoose/repositories/users/update-user.repository";
+import { UserController } from "./controllers/user.controller";
+import { CreateRegionUseCase } from "../application/use-case/region/create-region.use-case";
+import { CreateRegionRepositoryMongoAdapter } from "./database/orm/mongoose/repositories/regions/create-region.repository";
+import { RegionMapper } from "../application/common/mappers/region.mapper";
+import { DeleteRegionUseCase } from "../application/use-case/region/delete-region.use-case";
+import { DeleteRegionRepositoryMongoAdapter } from "./database/orm/mongoose/repositories/regions/delete-region.repository";
+import { GetRegionUseCase } from "../application/use-case/region/get-region.use-case";
+import { GetRegionRepositoryMongoAdapter } from "./database/orm/mongoose/repositories/regions/get-region.repository";
+import { GetAllRegionsUseCase } from "../application/use-case/region/get-all-regions.use-case";
+import { GetAllRegionsRepositoryMongoAdapter } from "./database/orm/mongoose/repositories/regions/get-all-regions.repository";
+import { UpdateRegionUseCase } from "../application/use-case/region/update-region.use-case";
+import { UpdateRegionRepositoryMongoAdapter } from "./database/orm/mongoose/repositories/regions/update-region.repository";
+import { GeospatialProximityUseCase } from "../application/use-case/region/geospatial-proximity.use-case";
+import { PointContainedInRegionUseCase } from "../application/use-case/region/point-contained-in-region.use-case";
+import { PointContainedInRegionRepositoryMongoAdapter } from "./database/orm/mongoose/repositories/regions/point-contained-in-region.repository";
+import { GeospatialProximityRepositoryMongoAdapter } from "./database/orm/mongoose/repositories/regions/geospatial-proximity.repository";
+import { RegionController } from "./controllers/region.controller";
+import { logger } from "./providers/logger/logger";
+import { AuthMiddleware } from "./middlewares/auth-validation.middleware";
+
+import { Express } from "express";
+
+export class GeoAppModule {
+  private container = Container;
+  private _server: Express;
+
+  constructor(server: Express) {
+    this._server = server;
+  }
+
+  public configureServices() {
+    const createUserUsecase = new CreateUserUseCase(
+      new CreateUserRepositoryMongoAdapter(),
+      new GeoLibOpenStreetMapAdapter(
+        new NetworkAxiosAdapter("https://nominatim.openstreetmap.org"),
+        logger
+      ),
+      new UserMapper(),
+      new AuthProvider()
+    );
+
+    const deleteUserUsecase = new DeleteUserUseCase(
+      new DeleteUserRepositoryMongoAdapter()
+    );
+
+    const getUserUsecase = new GetUserUseCase(
+      new GetUserRepositoryMongoAdapter()
+    );
+
+    const getAllUsersUsecase = new GetAllUsersUseCase(
+      new GetAllUsersRepositoryMongoAdapter()
+    );
+
+    const updateUserUsecase = new UpdateUserUseCase(
+      new UpdateUserRepositoryMongoAdapter(),
+      new UserMapper(),
+      new GeoLibOpenStreetMapAdapter(
+        new NetworkAxiosAdapter("https://nominatim.openstreetmap.org"),
+        logger
+      )
+    );
+
+    this.container.set(
+      UserController,
+      new UserController(
+        createUserUsecase,
+        deleteUserUsecase,
+        getUserUsecase,
+        getAllUsersUsecase,
+        updateUserUsecase
+      )
+    );
+
+    const createRegionUsecase = new CreateRegionUseCase(
+      new CreateRegionRepositoryMongoAdapter(),
+      new RegionMapper()
+    );
+
+    const deleteRegionUsecase = new DeleteRegionUseCase(
+      new DeleteRegionRepositoryMongoAdapter()
+    );
+
+    const getRegionUsecase = new GetRegionUseCase(
+      new GetRegionRepositoryMongoAdapter()
+    );
+
+    const getAllRegionsUsecase = new GetAllRegionsUseCase(
+      new GetAllRegionsRepositoryMongoAdapter()
+    );
+
+    const updateRegionUsecase = new UpdateRegionUseCase(
+      new UpdateRegionRepositoryMongoAdapter(),
+      new RegionMapper()
+    );
+
+    const pointContainedInRegionUsecase = new PointContainedInRegionUseCase(
+      new PointContainedInRegionRepositoryMongoAdapter()
+    );
+
+    const geospatialProximityUsecase = new GeospatialProximityUseCase(
+      new GeospatialProximityRepositoryMongoAdapter()
+    );
+
+    this.container.set(
+      RegionController,
+      new RegionController(
+        createRegionUsecase,
+        deleteRegionUsecase,
+        getRegionUsecase,
+        getAllRegionsUsecase,
+        updateRegionUsecase,
+        pointContainedInRegionUsecase,
+        geospatialProximityUsecase
+      )
+    );
+
+    this.container.set(AuthMiddleware, new AuthMiddleware());
+
+    useContainer(this.container)
+  }
+
+  public configureServer() {
+    useExpressServer(this._server, {
+      controllers: [UserController, RegionController],
+    });
+  }
+
+  public getContainer(): Container {
+    return this.container;
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -3,40 +3,7 @@ import * as dotenv from "dotenv";
 import { validateCheckZodVariables } from "./shared/env.validation";
 import { MongoBoostrap } from "./geo-app/infra/database/orm/mongoose";
 import { logger } from "./geo-app/infra/providers/logger/logger";
-import { CreateUserUseCase } from "./geo-app/application/use-case/user/create-user.use-case";
-import { CreateUserRepositoryMongoAdapter } from "./geo-app/infra/database/orm/mongoose/repositories/users/create-user.repository";
-import { GeoLibOpenStreetMapAdapter } from "./geo-app/infra/providers/geo/geo-lib.provider";
-import { NetworkAxiosAdapter } from "./shared/network.provider";
-import { useContainer, useExpressServer } from "routing-controllers";
-import Container from "typedi";
-import { DeleteUserUseCase } from "./geo-app/application/use-case/user/delete-user-use-case";
-import { DeleteUserRepositoryMongoAdapter } from "./geo-app/infra/database/orm/mongoose/repositories/users/delete-user.repository";
-import { GetUserUseCase } from "./geo-app/application/use-case/user/get-user.use-case";
-import { GetUserRepositoryMongoAdapter } from "./geo-app/infra/database/orm/mongoose/repositories/users/get-user.repository";
-import { GetAllUsersUseCase } from "./geo-app/application/use-case/user/get-all-users.use-case";
-import { GetAllUsersRepositoryMongoAdapter } from "./geo-app/infra/database/orm/mongoose/repositories/users/get-all-users.repository";
-import { UpdateUserUseCase } from "./geo-app/application/use-case/user/update-user.use-case";
-import { UpdateUserRepositoryMongoAdapter } from "./geo-app/infra/database/orm/mongoose/repositories/users/update-user.repository";
-import { UserMapper } from "./geo-app/application/common/mappers/user.mapper";
-import { UserController } from "./geo-app/infra/controllers/user.controller";
-import { RegionController } from "./geo-app/infra/controllers/region.controller";
-import { CreateRegionUseCase } from "./geo-app/application/use-case/region/create-region.use-case";
-import { RegionMapper } from "./geo-app/application/common/mappers/region.mapper";
-import { CreateRegionRepositoryMongoAdapter } from "./geo-app/infra/database/orm/mongoose/repositories/regions/create-region.repository";
-import { DeleteRegionUseCase } from "./geo-app/application/use-case/region/delete-region.use-case";
-import { DeleteRegionRepositoryMongoAdapter } from "./geo-app/infra/database/orm/mongoose/repositories/regions/delete-region.repository";
-import { GetRegionUseCase } from "./geo-app/application/use-case/region/get-region.use-case";
-import { GetRegionRepositoryMongoAdapter } from "./geo-app/infra/database/orm/mongoose/repositories/regions/get-region.repository";
-import { GetAllRegionsUseCase } from "./geo-app/application/use-case/region/get-all-regions.use-case";
-import { GetAllRegionsRepositoryMongoAdapter } from "./geo-app/infra/database/orm/mongoose/repositories/regions/get-all-regions.repository";
-import { UpdateRegionUseCase } from "./geo-app/application/use-case/region/update-region.use-case";
-import { UpdateRegionRepositoryMongoAdapter } from "./geo-app/infra/database/orm/mongoose/repositories/regions/update-region.repository";
-import { PointContainedInRegionUseCase } from "./geo-app/application/use-case/region/point-contained-in-region.use-case";
-import { PointContainedInRegionRepositoryMongoAdapter } from "./geo-app/infra/database/orm/mongoose/repositories/regions/point-contained-in-region.repository";
-import { GeospatialProximityRepositoryMongoAdapter } from "./geo-app/infra/database/orm/mongoose/repositories/regions/geospatial-proximity.repository";
-import { GeospatialProximityUseCase } from "./geo-app/application/use-case/region/geospatial-proximity.use-case";
-import { AuthProvider } from "./shared/auth.provider";
-import { AuthMiddleware } from "./geo-app/infra/middlewares/auth-validation.middleware";
+import { GeoAppModule } from "./geo-app/infra/geo-app.module";
 
 dotenv.config();
 
@@ -51,102 +18,13 @@ const config = {
 validateCheckZodVariables(config);
 
 const mongo = MongoBoostrap.getInstance();
-
-const createUserUsecase = new CreateUserUseCase(
-  new CreateUserRepositoryMongoAdapter(),
-  new GeoLibOpenStreetMapAdapter(
-    new NetworkAxiosAdapter("https://nominatim.openstreetmap.org"),
-    logger
-  ),
-  new UserMapper(),
-  new AuthProvider()
-);
-
-const deleteUserUsecase = new DeleteUserUseCase(
-  new DeleteUserRepositoryMongoAdapter()
-);
-
-const getUserUsecase = new GetUserUseCase(new GetUserRepositoryMongoAdapter());
-
-const getAllUsersUsecase = new GetAllUsersUseCase(
-  new GetAllUsersRepositoryMongoAdapter()
-);
-
-const updateUserUsecase = new UpdateUserUseCase(
-  new UpdateUserRepositoryMongoAdapter(),
-  new UserMapper(),
-  new GeoLibOpenStreetMapAdapter(
-    new NetworkAxiosAdapter("https://nominatim.openstreetmap.org"),
-    logger
-  )
-);
-
-Container.set(
-  UserController,
-  new UserController(
-    createUserUsecase,
-    deleteUserUsecase,
-    getUserUsecase,
-    getAllUsersUsecase,
-    updateUserUsecase
-  )
-);
-
-const createRegionUsecase = new CreateRegionUseCase(
-  new CreateRegionRepositoryMongoAdapter(),
-  new RegionMapper()
-);
-
-const deleteRegionUsecase = new DeleteRegionUseCase(
-  new DeleteRegionRepositoryMongoAdapter()
-);
-
-const getRegionUsecase = new GetRegionUseCase(
-  new GetRegionRepositoryMongoAdapter()
-);
-
-const getAllRegionsUsecase = new GetAllRegionsUseCase(
-  new GetAllRegionsRepositoryMongoAdapter()
-);
-
-const updateRegionUsecase = new UpdateRegionUseCase(
-  new UpdateRegionRepositoryMongoAdapter(),
-  new RegionMapper()
-);
-
-const pointContainedInRegionUsecase = new PointContainedInRegionUseCase(
-  new PointContainedInRegionRepositoryMongoAdapter()
-);
-
-const geospatialProximityUsecase = new GeospatialProximityUseCase(
-  new GeospatialProximityRepositoryMongoAdapter()
-);
-
-Container.set(
-  RegionController,
-  new RegionController(
-    createRegionUsecase,
-    deleteRegionUsecase,
-    getRegionUsecase,
-    getAllRegionsUsecase,
-    updateRegionUsecase,
-    pointContainedInRegionUsecase,
-    geospatialProximityUsecase
-  )
-);
-
-Container.set(AuthMiddleware, new AuthMiddleware());
-
-useContainer(Container);
-
 const server = app();
 const router = app.Router();
-
 server.use(app.json());
 
-useExpressServer(server, {
-  controllers: [UserController, RegionController],
-});
+const geoAppModule = new GeoAppModule(server);
+geoAppModule.configureServices();
+geoAppModule.configureServer();
 
 server.use(router);
 
