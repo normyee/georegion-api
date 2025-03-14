f290ea583a098e6890b4bad735de1cd96a31f9b8
normyee <lunoqro@gmail.com>
2025-03-14 19:32:11 -0300

    chore: remove logs from login use case

--- src/geo-app/application/use-case/user/login-user.use-case.ts
+++ src/geo-app/application/use-case/user/login-user.use-case.ts
@@ -8,17 +8,11 @@ export class LoginUserUseCase {
   ) {}
 
   async execute(email: string) {
-    console.log("passei 1 app");
     const user = await this._getUserByEmailRepository.execute(email);
-
-    console.log("passei 2 app");
 
     if (!user) return null;
 
-    console.log("passei 3 app");
-
     const token = this._authProvider.tokenize(user.id);
-    console.log("passei 4 app");
 
     return token;
   }
